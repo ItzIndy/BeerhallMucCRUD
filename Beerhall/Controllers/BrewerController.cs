aa3b052c929803294da265c125791fc4a7dc1571
Stefaan Samyn <stefaan.samyn@hogent.be>
2018-09-19 17:45:29 +0200

    Add success messages using TempData

--- Beerhall/Controllers/BrewerController.cs
+++ Beerhall/Controllers/BrewerController.cs
@@ -39,6 +39,7 @@ namespace Beerhall.Controllers
             Brewer brewer = _brewerRepository.GetBy(id);
             MapBrewerEditViewModelToBrewer(brewerEditViewModel, brewer);
             _brewerRepository.SaveChanges();
+            TempData["message"] = $"You successfully updated brewer {brewer.Name}.";
             return RedirectToAction(nameof(Index));
         }
 
@@ -56,6 +57,7 @@ namespace Beerhall.Controllers
             MapBrewerEditViewModelToBrewer(brewerEditViewModel, brewer);
             _brewerRepository.Add(brewer);
             _brewerRepository.SaveChanges();
+            TempData["message"] = $"You successfully added brewer {brewer.Name}.";
             return RedirectToAction(nameof(Index));
         }
 
@@ -68,8 +70,10 @@ namespace Beerhall.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            _brewerRepository.Delete(_brewerRepository.GetBy(id));
+            Brewer brewer = _brewerRepository.GetBy(id);
+            _brewerRepository.Delete(brewer);
             _brewerRepository.SaveChanges();
+            TempData["message"] = $"You successfully deleted brewer {brewer.Name}.";
             return RedirectToAction(nameof(Index));
         }
 
